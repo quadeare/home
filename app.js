1296b9581a65765d9ac57c85ecbd14e7c3fe9235
Florian Lacrampe <quadeare@users.noreply.github.com>
2019-12-23 22:19:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const app = new Vue({
     el: '#app',
     data: {
         config: null,
-        offline: false,
         filter: '',
         vlayout: true,
         isDark: null
@@ -17,31 +16,12 @@ const app = new Vue({
             this.vlayout = JSON.parse(localStorage.vlayout)
         }
         
-        this.checkOffline();
         that.getConfig().then(function (config) {
             that.config = config;
-        }).catch(function () {
-            that.offline = true;
         });
 
-        document.addEventListener('visibilitychange', function () {
-            if (document.visibilityState == "visible") {
-                that.checkOffline();
-            }
-        }, false);
     },
     methods: {
-        checkOffline: function () {
-            let that = this;
-            return fetch(window.location.href + "?alive", {
-                method: 'HEAD',
-                cache: 'no-store'
-            }).then(function () {
-                that.offline = false;
-            }).catch(function () {
-                that.offline = true;
-            });
-        },
         getConfig: function (event) {
             return fetch('config.yml').then(function (response) {
                 if (response.status != 200) {
